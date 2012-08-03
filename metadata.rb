3a4d4ff8caaa209b13dbdcfc49093c8d61735284
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-03 12:13:04 -0700

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ maintainer       "Chris Roberts"
 maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures s3fs-fuse"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.6"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.7"
 
 supports 'ubuntu'
 depends 'bluepill'
