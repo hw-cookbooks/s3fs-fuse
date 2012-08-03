849e6c74f0d54bbe732067b716b676d09c8409c9
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-03 12:11:55 -0700

    Provide better bluepill integration

--- recipes/bluepill.rb
+++ recipes/bluepill.rb
@@ -1,34 +1,36 @@
 include_recipe "bluepill"
 
+if(File.exists?('/etc/init/s3fs-fuse.conf'))
+  service "s3fs-fuse" do
+    provider Chef::Provider::Service::Upstart
+    action :stop
+  end
+
+  file '/etc/init/s3fs-fuse.conf' do
+    action :delete
+  end
+end
+
 mounted_directories = node['s3fs-fuse'][:mounts]
 if(mounted_directories.is_a?(Hash) || !mounted_directories.respond_to?(:each))
   mounted_directories = [node['s3fs-fuse'][:mounts]].compact
 end
 
-template "/etc/bluepill/s3fs.pill" do
-  source "bluepill-s3fs.erb"
-  variables(
-    :mounted_directories => mounted_directories,
-    :maxmemory => node['s3fs-fuse'][:maxmemory]
-  )
-  notifies :restart, 'service[s3fs-bluepill]'
-end
-
-template "/etc/init/s3fs-fuse.conf" do
-  source "upstart-s3fs.erb"
-  notifies :stop, 'service[s3fs-fuse]'
-  notifies :start, 'service[s3fs-fuse]'
-end
-
-service "s3fs-bluepill" do
-  start_command "bluepill s3fs start"
-  stop_command "bluepill s3fs stop"
-  restart_command "bluepill s3fs restart"
+execute 's3fs[bluepill-forced-load]' do
+  command "bluepill load #{File.join(node[:bluepill][:conf_dir], 's3fs.pill')}"
   action :nothing
 end
 
-service "s3fs-fuse" do
-  provider Chef::Provider::Service::Upstart
-  supports :status => true, :restart => true, :reload => true
-  action [:enable, :start]
+template File.join(node[:bluepill][:conf_dir], 's3fs.pill') do
+  source "bluepill-s3fs.erb"
+  variables(
+    :mounted_directories => mounted_directories,
+    :maxmemory => node['s3fs-fuse'][:maxmemory],
+    :pid_dir => File.join(node[:bluepill][:pid_dir], 'pids')
+  )
+  notifies :run, resources(:execute => 's3fs[bluepill-forced-load]'), :immediately
+end
+
+bluepill_service 's3fs' do
+  action [:enable, :load, :start]
 end
