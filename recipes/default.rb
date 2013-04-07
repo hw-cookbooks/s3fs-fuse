24f977867a0872af8605f863645518f8cb1d4dbc
tily <tily05@gmail.com>
2013-04-08 00:20:34 +0900

    move s3 endpoint url to attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,7 +28,7 @@ unless(node['s3fs-fuse'][:bluepill])
       fstype 'fuse'
       dump 0
       pass 0
-      options "allow_other,url=https://s3.amazonaws.com,passwd_file=/etc/passwd-s3fs,use_cache=#{dir_info[:tmp_store] || '/tmp/s3_cache'},retries=20#{",noupload" if dir_info[:no_upload]},#{dir_info[:read_only] ? 'ro' : 'rw'}"
+      options "allow_other,url=#{node['s3fs-fuse'][:s3_url]},passwd_file=/etc/passwd-s3fs,use_cache=#{dir_info[:tmp_store] || '/tmp/s3_cache'},retries=20#{",noupload" if dir_info[:no_upload]},#{dir_info[:read_only] ? 'ro' : 'rw'}"
       action [:mount, :enable]
       not_if "mountpoint -q #{dir_info[:path]}"
     end
