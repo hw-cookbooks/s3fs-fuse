b895eba5f5088ad3fbc50a6920f4e94e86cac490
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-17 21:19:14 -0800

    Include support for rc_mon based monitoring

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 
-mounted_directories = node['s3fs-fuse'][:mounts]
+mounted_directories = node[:s3fs_fuse][:mounts]
 if(mounted_directories.is_a?(Hash) || !mounted_directories.respond_to?(:each))
-  mounted_directories = [node['s3fs-fuse'][:mounts]].compact
+  mounted_directories = [node[:s3fs_fuse][:mounts]].compact
 end
 
 mounted_directories.each do |mount_point|
@@ -18,22 +18,20 @@ end
 
 include_recipe "s3fs-fuse::install"
 
-if(node['s3fs-fuse'][:bluepill])
-  include_recipe "s3fs-fuse::bluepill"
-end
-
-unless(node['s3fs-fuse'][:bluepill])
+if(node[:s3fs_fuse][:bluepill])
+  include_recipe 's3fs-fuse::bluepill'
+elsif(node[:s3fs_fuse][:rc_mon])
+  include_recipe 's3fs-fuse::rc_mon'
+else
   mounted_directories.each do |dir_info|
     mount dir_info[:path] do
       device "s3fs##{dir_info[:bucket]}"
       fstype 'fuse'
       dump 0
       pass 0
-      options "allow_other,url=#{node['s3fs-fuse'][:s3_url]},passwd_file=/etc/passwd-s3fs,use_cache=#{dir_info[:tmp_store] || '/tmp/s3_cache'},retries=20#{",noupload" if dir_info[:no_upload]},#{dir_info[:read_only] ? 'ro' : 'rw'}"
+      options "allow_other,url=#{node[:s3fs_fuse][:s3_url]},passwd_file=/etc/passwd-s3fs,use_cache=#{dir_info[:tmp_store] || '/tmp/s3_cache'},retries=20#{",noupload" if dir_info[:no_upload]},#{dir_info[:read_only] ? 'ro' : 'rw'}"
       action [:mount, :enable]
       not_if "mountpoint -q #{dir_info[:path]}"
     end
   end
 end
-
-
---
+++ recipes/rc_mon.rb
@@ -0,0 +1,21 @@
+
+mounted_directories.each do |mount|
+  mount_command = ["s3fs -f #{mount[:bucket]} #{mount[:path]} -o allow_other"]
+  mount_command << "url=#{File.join(node[:s3fs_fuse][:s3_url]}"
+  mount_command << "passwd_file=/etc/passwd-s3fs"
+  mount_command << "use_cache=#{mount[:tmp_store] || '/tmp/s3_cache'}"
+  mount_command << "retries=20"
+  mount_command << "dev"
+  mount_command << "suid"
+  if(mount[:no_upload])
+    mount_command << "noupload"
+  end
+  mount_command << mount[:read_only] ? 'ro' : 'rw'
+
+  rc_mon_service "s3fs-#{mount[:path].gsub('/', '-')}" do
+    start_command mount_command.join(',')
+    stop_command "umount #{mount[:path]}"
+    memory_limit "#{mount[:maxmemory]}M"
+  end
+
+end
