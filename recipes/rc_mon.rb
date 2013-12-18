d4a4f925bcc110de86f422ef3aeea6f63c930369
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-17 21:24:17 -0800

    Include close paren to fix syntax

--- recipes/rc_mon.rb
+++ recipes/rc_mon.rb
@@ -1,7 +1,7 @@
 
 mounted_directories.each do |mount|
   mount_command = ["s3fs -f #{mount[:bucket]} #{mount[:path]} -o allow_other"]
-  mount_command << "url=#{File.join(node[:s3fs_fuse][:s3_url]}"
+  mount_command << "url=#{File.join(node[:s3fs_fuse][:s3_url])}"
   mount_command << "passwd_file=/etc/passwd-s3fs"
   mount_command << "use_cache=#{mount[:tmp_store] || '/tmp/s3_cache'}"
   mount_command << "retries=20"
