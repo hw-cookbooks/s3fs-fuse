37fc2c1e98b34c693b654cc806672c8e061e6a1b
James Robinson <jtrobinson@meistermedia.com>
2014-10-28 16:29:01 -0400

    Added check for "fuse-utils" package with fallback to fuse for newer debian versions.

--- recipes/install.rb
+++ recipes/install.rb
@@ -11,31 +11,30 @@ template '/etc/passwd-s3fs' do
   mode 0600
 end
 
-prereqs = case node.platform_family
-when 'debian'
-  %w(
-    build-essential
-    libfuse-dev
-    fuse-utils
-    libcurl4-openssl-dev
-    libxml2-dev
-    mime-support
-  )
-when 'rhel'
-  %w(
-    gcc
-    libstdc++-devel
-    gcc-c++
-    curl-devel
-    libxml2-devel
-    openssl-devel
-    mailcap
-    fuse
-    fuse-devel
-    fuse-libs
-  )
-else
-  raise "Unsupported platform family provided: #{node.platform_family}"
+prereqs = []
+
+case node.platform_family
+  when 'debian'
+    prereqs.push(
+      'build-essential', 'libfuse-dev', 'libcurl4-openssl-dev', 'libxml2-dev',
+      'mime-support'
+    )
+    # As of Ubuntu 14.04 "fuse-utils" has been merged into package "fuse"
+    # so try to install both (auto-installed by fuse-utils in older)
+    %x(sudo apt-cache show fuse-utils)
+    if( $? == 0 ) then
+      prereqs.push( 'fuse-utils' )
+    else
+      prereqs.push( 'fuse' )
+    end
+  when 'rhel'
+    prereqs.push(
+      'gcc', 'libstdc++-devel', 'gcc-c++', 'curl-devel', 'libxml2-devel',
+      'libxml2-devel', 'openssl-devel', 'mailcap', 'fuse', 'fuse-devel',
+      'fuse-libs'
+    )
+  else
+    raise "Unsupported platform family provided: #{node.platform_family}"
 end
 
 prereqs = node[:s3fs_fuse][:packages] unless node[:s3fs_fuse][:packages].empty?
